29a2c8861fb523a7081ee4ab4137b2799092a5b9
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2025-05-26 16:11:34 +0200

    Add files via upload

--- L6 | SpaceInvaders/L5|spaceInvaders.py
+++ L6 | SpaceInvaders/L5|spaceInvaders.py
@@ -31,9 +31,10 @@ def draw ():
     screen.blit (healthTxt1, (10, 10))
     screen.blit (healthTxt2, (555, 10))
     if gameOver:
-        screen.blit (winTxt, (125, 225))
+        screen.blit (bg, (0, 0))
+        screen.blit (winTxt, (150, 225))
 
-def p1Move (keys):
+def p1Control (keys):
     if keys[py.K_w] and p1Rect.y > 10:
         p1Rect.y -= 1
     if keys[py.K_a] and p1Rect.x > 10:
@@ -43,26 +44,60 @@ def p1Move (keys):
     if keys[py.K_d] and p1Rect.x < 315:
         p1Rect.x += 1
 
-def p2Move (keys):
+def p2Control (keys):
     if keys[py.K_UP] and p2Rect.y > 10:
         p2Rect.y -= 1
     if keys[py.K_LEFT] and p2Rect.x > 435:
         p2Rect.x -= 1
     if keys[py.K_DOWN] and p2Rect.y < 735:
         p2Rect.y += 1
-    if keys[py.K_RIGHT] and p2Rect.x < 800:
+    if keys[py.K_RIGHT] and p2Rect.x < 740:
         p2Rect.x += 1
+
+def bulletsMove (bullets1, bullets2):
+    global health1, health2, winner, gameOver
+    for i in bullets1:
+        i.x += 1
+        if p2Rect.colliderect (i):
+            bullets1.remove (i)
+            health2 -= 1
+            if health2 == 0:
+                winner = "red"
+                gameOver = True
+                draw ()
+        elif i.x >= 800:
+            bullets1.remove (i)
+    for i in bullets2:
+        i.x -= 1
+        if p1Rect.colliderect (i):
+            bullets2.remove (i)
+            health1 -= 1
+            if health1 == 0:
+                winner = "yellow"
+                gameOver = True
+                draw ()
+        elif i.x <= 0:
+            bullets2.remove (i)
 
 while True:
     for event in py.event.get ():
         if event.type == py.QUIT:
             exit ()
-    screen.blit (bg, (0, 0))
-    screen.blit (p1, (p1Rect.x, p1Rect.y))
-    screen.blit (p2, (p2Rect.x, p2Rect.y))
-    draw ()
-    keys = py.key.get_pressed ()
-    p1Move (keys)
-    p2Move (keys)
-    py.draw.rect (screen, "black", border)
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_RSHIFT:
+                bulletRect = py.Rect (p2Rect.x, p2Rect.y + 25, 10, 5)
+                bullets2.append (bulletRect)
+            if event.key == py.K_LSHIFT:
+                bulletRect = py.Rect (p1Rect.x + 50, p1Rect.y + 25, 10, 5)
+                bullets1.append (bulletRect)
+    if not(gameOver):
+        screen.blit (bg, (0, 0))
+        screen.blit (p1, (p1Rect.x, p1Rect.y))
+        screen.blit (p2, (p2Rect.x, p2Rect.y))
+        py.draw.rect (screen, "black", border)
+        keys = py.key.get_pressed ()
+        p1Control (keys)
+        p2Control (keys)
+        draw ()
+    bulletsMove (bullets1, bullets2)
     py.display.update ()
