6c8759bb5d64e375c5b12c8daa332a5c4536f022
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2025-07-28 16:25:10 +0200

    Add files via upload

--- L9 | asteroids/L9|asteroids.py
+++ L9 | asteroids/L9|asteroids.py
@@ -1,4 +1,5 @@
 import pygame as py
+import random as r
 import math
 
 py.init ()
@@ -14,9 +15,6 @@ starTimer = 0
 bullets = []
 
 bg = py.image.load ("L9 | asteroids/images/bg.png")
-asteroidSmall = py.image.load ("L9 | asteroids/images/smallAsteroid.png")
-asteroidMedium = py.image.load ("L9 | asteroids/images/bigAsteroid.png")
-asteroidBig = py.image.load ("L9 | asteroids/images/megaAsteroid.png")
 ufo = py.image.load ("L9 | asteroids/images/UFO.png")
 star = py.image.load ("L9 | asteroids/images/star.png")
 ship = py.image.load ("L9 | asteroids/images/ship.png")
@@ -65,9 +63,6 @@ class Ship:
             else:
                 self.angle += 2.5
             player.updateRotation ()
-        if key[py.K_SPACE]:
-                bullet = Bullet (self.rotatedRect.x, self.rotatedRect.y, self.angle)
-                bullets.append (bullet)
         self.rotatedRect.center = self.x, self.y
 
 class Bullet:
@@ -75,7 +70,7 @@ class Bullet:
         self.x = x
         self.y = y
         self.dir = dir
-        self.radius = 10
+        self.radius = 10 
         self.speed = 5
         self.color = "red"
         self.dx = math.cos (math.radians (-self.dir)) * self.speed
@@ -86,17 +81,63 @@ class Bullet:
         self.x += self.dx
         self.y += self.dy
 
+class Asteroid:
+    def __init__ (self):
+        self.rock = r.randint (1, 3)
+        if self.rock == 1:
+           self.image = py.image.load ("L9 | asteroids/images/smallAsteroid.png")
+           self.size = 50
+        if self.rock == 2:
+           self.image = py.image.load ("L9 | asteroids/images/mediumAsteroid.png")
+           self.size = 100
+        if self.rock == 3:
+           self.image = py.image.load ("L9 | asteroids/images/bigAsteroid.png")
+           self.size = 150
+        self.edge = r.randint (1, 4)
+        if self.edge == 1:
+            self.x = r.randint (0, 800)
+            self.y = -100
+        if self.edge == 2:
+            self.x = 900
+            self.y = r.randint (0, 800)
+        if self.edge == 3:
+            self.x = r.randint(0, 800)
+            self.y = 900
+        if self.edge == 4:
+            self.x = -100
+            self.y = r.randint (0, 800)
+        self.rect = py.Rect (self.x, self.y, self.size, self.size)
+        self.speed = r.uniform (1.5, 3.0)
+    def move (self):
+        dx = (self.x - 400) * -1 - self.rect.centerx
+        dy = (self.y - 400) * -1 - self.rect.centery
+        distance = (dx ** 2 + dy ** 2) ** 0.5
+        self.vx = self.speed * dx / distance
+        self.vy = self.speed * dy / distance
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+    def draw (self):
+        screen.blit (self.image, (self.rect.x, self.rect.y))
+
+asteroids = [Asteroid () for i in range (5)]
 player = Ship ()
 
 while True:
+    screen.blit (bg, (0, 0))
     for event in py.event.get ():
         if event.type == py.QUIT:
             exit ()
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_SPACE:
+                bullet = Bullet (player.rotatedRect.centerx, player.rotatedRect.centery, player.angle + 90)
+                bullets.append (bullet)
+    for i in asteroids:
+        i.move ()
+        i.draw ()
     for bul in bullets:
         bul.move ()
         bul.draw ()
     key = py.key.get_pressed ()
     player.move (key)
-    screen.blit (bg, (0, 0))
     player.draw (screen)
     py.display.update ()
